fcb92707d57069028789f9fe7178401cf90394a2
Iamneo <techadmin@iamneo.ai>
2023-08-01 13:20:50 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,54 +1,87 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import Banner from "./components/UI/Banner/Banner";
 
 function App() {
-// State variables to control the start, attempt, question, card and result of the Quiz
-const [start, setStart] = useState(true);
-const [attempt, correctAnswerMarkUpdate] = useState(0);
-const [qsnAttempt, setqsnAttempt] = useState(0);
-const [card, setCard] = useState(false);
-const [result, setResult] = useState(false)
-const [banner,setBanner] = useState(false)
-// Event handler to start the Quiz
-const handleStart = () => {
-setStart(false)
-setCard(true)
-correctAnswerMarkUpdate(0)
-setqsnAttempt(0)
-setBanner(false)
+  // State variables to control the start, attempt, question, card, and result of the Quiz
+  const [start, setStart] = useState(true);
+  const [attempt, setAttempt] = useState(0);
+  const [qsnAttempt, setQsnAttempt] = useState(0);
+  const [showCard, setShowCard] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [showBanner, setShowBanner] = useState(false);
+
+  // Event handler to start the Quiz
+  const handleStart = () => {
+    setStart(false);
+    setShowCard(true);
+    setAttempt(0);
+    setQsnAttempt(0);
+    setShowBanner(false);
+  };
+
+  // Options for the quiz
+  const options = {
+    option1: "Blue",
+    option2: "Red",
+    option3: "Yellow",
+    option4: "Green",
+  };
+
+  // Array of questions
+  const questions = [
+    { question: "What color are the leaves?", answer: "Green" },
+    { question: "What color is the sky?", answer: "Blue" },
+    { question: "What color is a banana?", answer: "Yellow" },
+    { question: "What color is a strawberry?", answer: "Red" },
+    { question: "What color is a watermelon?", answer: "Green" },
+  ];
+
+  // Event handler to show the result of the Quiz
+  const handleResult = () => {
+    setStart(true);
+    setShowBanner(true);
+    setShowCard(false);
+    setShowResult(false);
+  };
+
+  return (
+    <div className="flex justify-center mt-4">
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <div>
+          <h1 className="text-3xl">Quizz App</h1>
+          <h1 style={{ textAlign: "center" }}>learn react</h1>
+        </div>
+        {showBanner && <Banner score={attempt} />}
+        {showCard && (
+          <div>
+            {questions.map((q, index) => (
+              <Card
+                key={index}
+                question={q.question}
+                setAttempt={setAttempt}
+                attempt={attempt}
+                options={options}
+                answer={q.answer}
+                setQsnAttempt={setQsnAttempt}
+                qsn={qsnAttempt}
+                setShowCard={setShowCard}
+                setShowResult={setShowResult}
+                qsnAttempt={qsnAttempt}
+              />
+            ))}
+          </div>
+        )}
+        {showResult && (
+          <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"}>
+            Show Results
+          </Button>
+        )}
+        {start && <Button onClick={handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button>}
+      </div>
+    </div>
+  );
 }
-// Options for the quiz
-const opt = {
-option1 : "Blue",option2 : "Red",option3 : "Yellow",option4 : "Green"
-}
-// Array of questions
-const questions = [ { question: "What color are the leaves?", answer: "Green" }, { question: "What color is the sky?", answer: "Blue" }, { question: "What color is a banana?", answer: "Yellow" }, { question: "What coloris a strawberry?", answer: "Red" }, { question: "What color is a watermelon?", answer: "Green" } ];
-// Event handler to show the result of the Quiz
-const handleResult = () => {
-setStart(true)
-setBanner(true)
-setCard(false)
-setResult(false)
-}
-return (
-<div className="flex justify-center mt-4 ">
-<div style={{display:'flex' ,justifyContent:'center',alignItems:'center', flexDirection:'column'}}>
-<div >
-<h1 className="text-3xl">Quizz App</h1>
-<h1 style={{textAlign:'center'}}>learn react</h1>
-</div>
-{/* {start && <Button onClick={ handleStart} style={"bg-orange-
-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button> } */}
-{banner && <Banner score={attempt} />}
-{card && <div> {questions.map((q, index) => (
-<Card key={index} question={q.question} correctAnswerMarkUpdate={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer}setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />))}
-</div>}
-{result && <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
-{start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button> }
-</div>
-</div>
-);
-}
+
 export default App;
